9af5420a7492bd9749e52ca9a2a8c764f45b69b2
Kevin97125 <108547007+Kevin97125@users.noreply.github.com>
2022-08-18 11:24:06 -0400

    Update HallucineModel.class.php
    ok

--- www/models/HallucineModel.class.php
+++ www/models/HallucineModel.class.php
@@ -1,3 +1,19 @@
 <?php
 
+require_once "Model.class.php";
+
+class hallucineModel extends Model{
+    private $_movies;
+
+    public function requestMovies(){
+        $_movies = array(); 
+        $sql = "SELECT * FROM `movies` ORDER BY title;";//recuperation des filmes
+
+        $request = $this->getDatabase("localhost", "hallucine", "root", "")->prepare($sql);
+
+        $request->execute(); //execute la requetes
+        $rows = $request->fetchAll(PDO::FETCH_ASSOC);
+        $request->closeCursor(); //Fermeture de la requete
+    }
+}
 ?>
